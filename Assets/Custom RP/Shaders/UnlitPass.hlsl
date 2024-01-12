5855aa92bdb2daa1c00d74d41dc49f0ea3c910e6
AGXYE <133179396+AGXYE@users.noreply.github.com>
2024-01-12 16:53:26 +0800

    Unlit Shader
    UnlitShader common.hlsl unityinput.hlsl

---
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -0,0 +1,20 @@
+﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDE
+#define CUSTOM_UNLIT_PASS_INCLUDE
+#include "../ShaderLibrary/Common.hlsl"
+float4 _BaseColor;
+float4 UnlitPassVertex(float3 positionOS: POSITION): SV_POSITION
+{
+    float3 positionWS = TransformObjectToWorld(positionOS);
+	return TransformWorldToHClip(positionWS);
+}
+
+float4 UnlitPassFragment():SV_TARGET
+{
+    return _BaseColor;   
+}
+
+
+
+
+
+#endif
